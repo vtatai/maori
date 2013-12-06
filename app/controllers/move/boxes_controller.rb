84f69f1aebaff88ecbbf57a4106a225c6569fabd
Victor Tatai <victor.tatai@appdirect.com>
2013-12-06 17:17:27 -0300

    Implementing box crud

--- app/controllers/move/boxes_controller.rb
+++ app/controllers/move/boxes_controller.rb
@@ -9,5 +9,31 @@ module Move
     def show
       @box = Box.find(params[:id])
     end
+
+    def new
+      @box = Box.new
+    end
+
+    def create
+      @box = Box.new(params[:move_box])
+      if @box.save
+        redirect_to @box
+      else
+        render :action => 'new'
+      end
+    end
+
+    def edit
+      @box = Box.find(params[:id])
+    end
+
+    def update
+      @box = Box.find(params[:id])
+      if @box.update_attributes(params[:move_box])
+        redirect_to @box
+      else
+        redirect_to :edit
+      end
+    end
   end
 end
